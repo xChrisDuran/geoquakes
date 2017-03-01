8ac19eadb4ddbb2907baa2e53f78ec3a27b37dc5
xChrisDuran <chris.duran1205@gmail.com>
2017-03-01 14:35:34 -0800

    connected geo/earthquake api to js through jquery and ajax.

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,32 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
+
+// var onSuccess = function onSuccess(onJson) {
+//     console.log(onJson);
+//     $('#earthQuakeMonth').append(earthquakeMagnitude, earthquakeLocation);
+// }
+
+// var onError = function onError(onJson, status) {
+//     console.log(onJson, status);
+// }
+var monthlyQuakesEndpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 $(document).on("ready", function() {
 
-  // CODE IN HERE!
 
+
+    $.ajax({
+        method: 'GET',
+        url: monthlyQuakesEndpoint,
+        success: function onSuccess(onJson) {
+            console.log(onJson);
+            var earthquakeMagnitude = $('#earthQuakeMonth').append(onJson.features[0].properties.mag);
+            var earthquakeLocation = $('#earthQuakeMonth').append(onJson.features[0].properties.place);
+        },
+        forEach()
+        error: function onError(onJson, status) {
+            console.log(onJson, status);
+        }
+
+    });
 });
